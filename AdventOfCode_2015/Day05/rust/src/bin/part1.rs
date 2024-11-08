d5769aa99e7ced2484d45e5f2384ee9defdefb01
Cl1ngz <ByteIgma@gmail.com>
2024-11-08 15:37:11 +0100

    Day05 rust

--- AdventOfCode_2015/Day05/rust/src/bin/part1.rs
+++ AdventOfCode_2015/Day05/rust/src/bin/part1.rs
@@ -3,19 +3,19 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 fn is_nice(s: &str) -> bool {
-    // Rule 1: At least three vowels
+    // At least three vowels
     let vowels = "aeiou";
     let vowel_count = s.chars().filter(|&c| vowels.contains(c)).count();
     if vowel_count < 3 {
         return false;
     }
 
-    // Rule 2: At least one double letter
+    // At least one double letter
     if !s.chars().tuple_windows().any(|(a, b)| a == b) {
         return false;
     }
 
-    // Rule 3: No forbidden substrings
+    // No forbidden substrings
     let forbidden = ["ab", "cd", "pq", "xy"];
     for &f in &forbidden {
         if s.contains(f) {
@@ -42,8 +42,8 @@ fn main() -> io::Result<()> {
         .filter_map(|line| match line {
             Ok(l) => Some(l), // If line is Ok, continue
             Err(e) => {
-                eprintln!("Error reading line: {}", e); // Print the error to stderr
-                None // Skip this line
+                eprintln!("Error reading line: {}", e);
+                None
             }
         })
         .filter(|line| is_nice(line)) // Filter nice strings
--- AdventOfCode_2015/Day05/rust/src/bin/part2.rs
+++ AdventOfCode_2015/Day05/rust/src/bin/part2.rs
@@ -1,10 +1,44 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+
+fn contains_non_overlapping_pair(s: &str) -> bool {
+    for i in 0..s.len() - 1 {
+        for j in i + 2..s.len() - 1 {
+            if s[i..i + 2] == s[j..j + 2] {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn contains_repeat_with_one_between(s: &str) -> bool {
+    s.chars().tuple_windows().any(|(a, _, c)| a == c)
+}
+
+fn is_nice(s: &str) -> bool {
+    contains_repeat_with_one_between(s) && contains_non_overlapping_pair(s)
+}
 
 fn main() -> io::Result<()> {
     let path = "../input.txt";
     let file = File::open(path)?;
     let reader = BufReader::new(file);
+
+    let nice_count = reader
+        .lines() // This produces an iterator over Result<String, io::Error>
+        .filter_map(|line| match line {
+            Ok(l) => Some(l), // If line is Ok, continue
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                None
+            }
+        })
+        .filter(|line| is_nice(line)) // Filter nice strings
+        .count(); // Count how many nice strings there are
+
+    println!("Number of nice strings: {}", nice_count);
 
     Ok(())
 }
