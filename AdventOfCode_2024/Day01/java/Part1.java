cf4efe3998f37a4cae7fff63c95ce44b7aba56c1
Cl1ngz <ByteIgma@gmail.com>
2025-03-06 20:25:37 +0100

    .

--- AdventOfCode_2024/Day01/java/Part1.java
+++ AdventOfCode_2024/Day01/java/Part1.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Part1 {
     public static void main(String[] args) {
+        String filepath = "../input.txt";
+        List<Integer> firstList = new ArrayList<>();
+        List<Integer> secondList = new ArrayList<>();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] numb = line.trim().split("\\s+");
+                firstList.add(Integer.parseInt(numb[0]));
+                secondList.add(Integer.parseInt(numb[1]));
+            }
+
+            // Sort the lists
+            Collections.sort(firstList);
+            Collections.sort(secondList);
+
+            int distance = 0;
+            for (int i = 0; i < firstList.size(); i++) {
+                distance += Math.abs(firstList.get(i) - secondList.get(i));
+            }
+
+            System.out.println(distance);
+        } catch (IOException e) {
+            System.err.println("Error reading the input file: " + e.getMessage());
+        }
     }
 }
