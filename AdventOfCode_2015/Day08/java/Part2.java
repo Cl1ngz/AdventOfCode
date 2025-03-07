aa3c2dda8f89845f3636594b3ccb66c488b3c64a
Cl1ngz <ByteIgma@gmail.com>
2025-03-07 18:39:29 +0100

    Day 08 2015 part2

--- AdventOfCode_2015/Day08/java/Part2.java
+++ AdventOfCode_2015/Day08/java/Part2.java
@@ -6,12 +6,38 @@ import java.io.IOException;
 public class Part2 {
     public static void main(String[] args) {
         String filepath = "../input.txt";
-
+        int totalCodeLength = 0;
+        int totalEncodedLength = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                totalCodeLength += line.length();
 
+                String encoded = encodeString(line);
+                totalEncodedLength += encoded.length();
+
+            }
         } catch (IOException e) {
             System.err.println("Error reading the input file: " + e.getMessage());
         }
+
+        System.out.println("Difference: " + (totalEncodedLength - totalCodeLength));
+    }
+
+    private static String encodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('"');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '"' || c == '\\') {
+                sb.append('\\');
+            }
+            sb.append(c);
+        }
+
+        sb.append('"');
+        return sb.toString();
     }
 }
